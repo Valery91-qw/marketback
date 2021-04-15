40a719b823e63f0f3b7a0f76e501792c4128fb85
Valery91-qw <vakriv91@gmail.com>
2021-04-15 16:20:24 +0300

    add cors options

--- index.js
+++ index.js
@@ -10,7 +10,20 @@ const {getTotalProduct} = require("./repository/db");
 app.use(cors())
 // app.use(express.urlencoded({extended: true}))
 // app.use(express.json())
+let allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
+    // intercept OPTIONS method
+    if ('OPTIONS' === req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+};
+app.use(allowCrossDomain)
 app.get("/", async (request, response) => {
     const product = await getTotalProduct()
     if(product) {
