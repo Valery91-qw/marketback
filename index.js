137360fa947bcc8a7f4af18d80cefcad14f6e599
Valery91-qw <vakriv91@gmail.com>
2021-04-15 16:39:54 +0300

    add cors options

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ const express = require('express')
 const cors = require('cors')
 // создание объекта приложения
 const app = express()
+app.use(cors())
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
 //порт на котором происходит запуск приложения
 const port = process.env.PORT || 3004
 const {getTotalProduct} = require("./repository/db");
 
 
-app.get("/", cors(), async (request, response) => {
+app.get("/", async (request, response) => {
     const product = await getTotalProduct()
     if(product) {
         response.send(product)
